54879180037d797b65661ec81a7773b48fb4dfde
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2024-10-08 16:29:59 +0300

    INITIAL UPDATE

--- BLL/DAL/Product.cs
+++ BLL/DAL/Product.cs
@@ -20,11 +20,14 @@ public class Product
     
     
     // THIS IS THE C# WAY TO IMPLEMENT BASIC GET SET.
+    // In C# if you do not assign any values on numeric types,
+    // the default values will be 0.
     // Property
     public int Id { get; set; }
-    public string? Name { get; set; }          // In C#, if a variable can be nullable, then you should add question marks to make it nullable.
-    public double OldUnitPrice { get; set; }   // (Will not be used) Like in C++, in C# double is also precision dependent.
-    public decimal UnitPrice { get; set; }     // This is why it is better to use decimal type
+    public string Name { get; set; } = "unknown";    // In C#, if a variable can be nullable, then you should add question marks to make it nullable.
+    public double OldUnitPrice { get; set; }         // (Will not be used) Like in C++, in C# double is also precision dependent.
+    public decimal UnitPrice { get; set; }           // This is why it is better to use decimal type
     public DateTime ExpirationDate { get; set; }
     public int CategoryId { get; set; }
+    public int? StockAmount { get; set; }
 }
