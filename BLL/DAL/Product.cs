6d6ffaf451c7207b4676a70849304f3378a0cf03
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2024-10-08 16:18:38 +0300

    INITIAL UPDATE

--- BLL/DAL/Product.cs
+++ BLL/DAL/Product.cs
@@ -22,8 +22,8 @@ public class Product
     // THIS IS THE C# WAY TO IMPLEMENT BASIC GET SET.
     // Property
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public double oldUnitPrice { get; set; }   // (Will not be used) Like in C++, in C# double is also precision dependent.
+    public string? Name { get; set; }          // In C#, if a variable can be nullable, then you should add question marks to make it nullable.
+    public double OldUnitPrice { get; set; }   // (Will not be used) Like in C++, in C# double is also precision dependent.
     public decimal UnitPrice { get; set; }     // This is why it is better to use decimal type
     public DateTime ExpirationDate { get; set; }
     public int CategoryId { get; set; }
--- WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
+++ WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b0df4e0c7a52cc7d92785946dcbcf3300ba051")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
