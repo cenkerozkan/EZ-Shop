80b0df4e0c7a52cc7d92785946dcbcf3300ba051
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2024-10-08 15:58:48 +0300

    INITIAL UPDATE

---
+++ BLL/DAL/Product.cs
@@ -0,0 +1,30 @@
+using System.Reflection.Emit;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace BLL.DAL;
+public class Product
+{
+    // THIS IS THE JAVA VAY TO IMPLEMENT BASIC GET SET.
+    
+    // private int id;
+    //
+    // public void setId(int id)
+    // {
+    //     this.id = id;
+    // }
+    //
+    // public int getId()
+    // {
+    //     return this.id;
+    // }
+    
+    
+    // THIS IS THE C# WAY TO IMPLEMENT BASIC GET SET.
+    // Property
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double oldUnitPrice { get; set; }   // (Will not be used) Like in C++, in C# double is also precision dependent.
+    public decimal UnitPrice { get; set; }     // This is why it is better to use decimal type
+    public DateTime ExpirationDate { get; set; }
+    public int CategoryId { get; set; }
+}
