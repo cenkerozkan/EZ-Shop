a238802e1c480a0592f14f7f85777d2a90c2efc8
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2024-10-15 16:05:55 +0300

    ADD DB CONTEXT OPTIONS CLASS FOR DB CONNECTION

--- BLL/DAL/Db.cs
+++ BLL/DAL/Db.cs
@@ -6,4 +6,9 @@ public class Db : DbContext
 {
     public DbSet <Product> Products { get; set; }
     public DbSet <Category> Categories { get; set; }
+
+    public Db(DbContextOptions<Db> options) : base(options)
+    {
+        
+    }
 }
